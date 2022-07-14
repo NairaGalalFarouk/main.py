e567e054adf887c95096f3d13688efc3018f2e23
NairaGalalFarouk <101639855+NairaGalalFarouk@users.noreply.github.com>
2022-07-14 02:47:50 +0200

    naira

--- apiRequest.py
+++
@@ -1,5 +0,0 @@
-import requests
-import json
-with open("E:/python sql/json.json") as f:
-    data = json.load(f)
-print(data)
--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function()
-{  $(".skill-num span").each(function()
-   {
-      $(this).animate({
-       'width':$(this).data.("width")
-      },1000);
-   })
-})
